077ac69f5d4f516bce36039f305bd1c55cb226c6
RowenStipe <rowen.stipe@gmail.com>
2016-04-15 03:25:06 -0400

    Initial publication

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Discord-D20Bot
 The public source code of my Discord dicebot written in Python
+
+## Using
+This bot requires that you have a proper Discord bot account through https://discordapp.com/developers/applications/me
+ 
+ - Python 3.5.1
+ - - Additional libraries
+ - Git (You're on Github for christ sakes)
+ - A Discord account to use the bot
+
+ When you have python installed you'll need to install the following libraries using in your console:
+
+`pip3 install python-Levenshtein`
+`pip3 install fuzzywuzzy`
+`pip3 install git+https://github.com/Rapptz/discord.py@async`
+ 
+Now you're almost ready, before running the bot script you will need to modify the `token` value to match the your bot account token
+
+Your bot is now ready to start using discord.
+
+To get your bot onto different servers you'll need proper server permissions, and the `Client ID` on your bot's application page then go to https://discordapp.com/oauth2/authorize?&client_id={Your bot's Client ID here}&scope=bot to direct it to servers. This URL can be shared to bring your bot to other servers, if you don't want to run a bot of your own you can invite my D20Bot (if I'm right) with ttps://discordapp.com/oauth2/authorize?&client_id=169068074010542080&scope=bot but I don't guarantee him to be online all the time.
---
+++ diceroll.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+""" Discord D20Bot  
+    Copyright (C) 2016  RowenStipe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import discord
+from discord.ext import commands
+
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import random
+
+# Compairitor string for dice
+cdice = '000d000'
+# Token to change for bot
+token = '1YOUR2BOT3TOKEN5HERE4'
+
+description = "A more advanced dice rolling bot."
+
+bot = commands.Bot(description=description)
+
+@bot.event
+async def on_ready():
+    print('Loading the dice for:')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('~~~~~~~~~~~~~~~~~~~~~~')
+    
+    
+@bot.event
+async def on_message(message):
+    # start Dice magic
+    msg = message.content.split(' ')
+
+    if message.content.startswith('$help'):
+        await bot.send_message(message.channel,'To make me roll dice for you simply type: \n `[#of dice]d[# Dice] [± Modifier] [Optional Message here]` \n `1d20 +8 Performance Check` \n To have me decide something for your lazy self: \n `$decide This or That or This thing or That guy over there` using `or` to separate the choices')
+    
+    if message.content.startswith('$decide'):
+        
+        decide_str = message.content.replace('$decide ', '')
+        decide_lst = decide_str.split('or')
+        rng = random.choice(decide_lst)
+        decision = rng.strip()
+        
+        await bot.send_message(message.channel, '{0.author.mention} out of the choices between: *{1}* \n The decision is: *{2}*'.format(message, decide_str, decision))
+
+    # Detect if Dice formula is given with out command: Basic
+    if fuzz.ratio(cdice, msg[0]) >= 18:
+        # Check if only dice is given
+        if len(msg) < 2:
+            msg.insert(1, '0')
+            msg.insert(2, ' ')
+        # Check if missing optional message
+        if len(msg) < 3:
+            msg.insert(2, ' ')
+        mod = msg[1].replace("+", "")
+        psay = ' '.join(msg[2:])
+        try:
+            rollt, maxn = map(int, msg[0].split('d'))
+        except ValueError:
+            pass
+        
+        try:
+            i = 0
+            rolled = []
+            while (i < rollt):
+                rng = random.randint(1, maxn)
+                rolled.append(rng)
+                print('Rolled: {0}'.format(rolled))
+                i = i + 1
+            rtotal = ' '.join(str(rolled))
+            total = sum(rolled) + int(mod)
+            sayr = '**{0.author.mention} Rolled**_(d{1})_ **:** `{2} + {3}` _{4}_ \nTotal: `{5}`'.format(message, maxn, rtotal, mod, psay, total)
+            await bot.send_message(message.channel, sayr)
+        except UnboundLocalError:
+            pass
+            
+bot.run(token)
