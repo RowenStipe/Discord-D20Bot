dcd9b847e2a64ad7e12fb26e0b2bb1e419ad2cc8
RowenStipe <rowen.stipe@gmail.com>
2016-04-27 01:34:20 -0400

    added in roll limiters

--- diceroll.py
+++ diceroll.py
@@ -23,7 +23,7 @@ from fuzzywuzzy import process
 import random
 
 # Compairitor string for dice
-cdice = '000d000'
+cdice = '255d10000'
 # Token to change for bot
 token = '1YOUR2BOT3TOKEN5HERE4'
 
@@ -46,7 +46,7 @@ async def on_message(message):
 
     # $help command
     if message.content.startswith('$help'):
-        await bot.send_message(message.channel,'To make me roll dice for you simply type:\n`[#of dice]d[# Dice] [± Modifier] [Optional Message here]`\nor\n`1d20 +8 Performance Check`\n\nTo have me decide something for your lazy self:\n`$decide This or That or This thing or That guy over there` using `or` to separate the choices\n\nMy source code: `https://github.com/RowenStipe/Discord-D20Bot`')
+        await bot.send_message(message.channel,'To make me roll dice for you simply type:\n`[#of dice MAX:255]d[# Dice MAX:10000] [± Modifier] [Optional Message here]`\nor\n`1d20 +8 Performance Check`\n\nTo have me decide something for your lazy self:\n`$decide This or That or This thing or That guy over there` using `or` to separate the choices\n\nMy source code: `https://github.com/RowenStipe/Discord-D20Bot`')
     
     # Make a decision
     if message.content.startswith('$decide'):
@@ -58,7 +58,9 @@ async def on_message(message):
         await bot.send_message(message.channel, '{0.author.mention} out of the choices between: *{1}* \n The decision is: *{2}*'.format(message, decide_str, decision))
 
     # Detect if 1d20 formula is given
-    if fuzz.ratio(cdice, msg[0]) >= 18:
+    if fuzz.ratio(cdice, msg[0]) >= 20:
+        rollt = 0
+        maxn = 0
         # Check if only dice is given to toss
         if len(msg) < 2:
             msg.insert(1, '0')
@@ -71,21 +73,33 @@ async def on_message(message):
         try:
             rollt, maxn = map(int, msg[0].split('d')) # Decide what and how many times to roll
         except ValueError:
-            pass
-        
+            pass    
         try:
-            i = 0
-            rolled = [] # List of rolls
-            while (i < rollt):
-                rng = random.randint(1, maxn)
-                rolled.append(rng)
-                print('{0.author.mention} Rolled: {1}'.format(message, rolled)) # Says rolls in terminal for debug purposes
-                i = i + 1
-            rtotal = ' '.join(str(rolled)) # String of rolls made
-            total = sum(rolled) + int(mod) # Decide the total ammout rolled
-            sayr = '**{0.author.mention} Rolled**_(d{1})_ **:** `{2} + {3}` _{4}_ \nTotal: `{5}`'.format(message, maxn, rtotal, mod, psay, total) #  Markdown formatted string to say
-            await bot.send_message(message.channel, sayr)
-        except UnboundLocalError:
+            if rollt > 255:
+                await bot.send_message(message.channel, '**{0.author.mention}** You want to roll too many dice!'.format(message))
+                valueerror = 'Too many Dice'
+                raise ValueError('Too mand dice')
+            if maxn > 10000:
+                await bot.send_message(message.channel, '**{0.author.mention}** I don\'t have that high of dice to roll!'.format(message))
+                valueerror = 'Too high of dice'
+                raise ValueError('To high of dice')
+        except ValueError:
+            print(valueerror)
+        else:
+            try:
+                i = 0
+                rolled = [] # List of rolls
+                while (i < rollt):
+                    rng = random.randint(1, maxn)
+                    rolled.append(rng)
+                    i = i + 1
+                total = sum(rolled) + int(mod) # Decide the total ammout rolled
+            
+                sayr = '**{0.author.mention} Rolled**_(d{1})_ **:** `{2}` + {3} _{4}_ \nTotal: `{5}`'.format(message, maxn, rolled, mod, psay, total) #  Markdown formatted string to say
+                await bot.send_message(message.channel, sayr)
+            except ValueError:
+                pass
+        finally:
             pass
             
 bot.run(token)
